4d84e8d670ce8324bb9999feb7b8a6e489074ea1
MateoNicoud <mateo.nicoud@le-campus-numerique.fr>
2024-01-24 12:21:06 +0100

    v1.3

--- public/index.php
+++ public/index.php
@@ -1,2 +1,26 @@
 <?php
-echo "Bienvenue sur le blog";
+/*echo "Bienvenue sur le blog";
+if ($_GET["action"] == "lenomdelaction"){
+    include"../ressources/views/layouts/footer.php";
+} if ($_GET["action"]=false){
+    include "../ressources/views/errors/404.php";
+} else {
+    include '../ressources/views/errors/404.php';
+}
+*/
+$success = false;
+$route = array(
+    "../ressources/views/errors/404.php" => "action",
+    "../ressources/views/layouts/footer.php" => "hobbies",
+);
+$page = filter_input(INPUT_GET,"action", FILTER_SANITIZE_URL);
+foreach ($route as  $key => $value) {
+    if ($page == $value) {
+        include $key;
+        $success= true;
+
+    }
+
+} if(!$success){
+    include '../ressources/views/errors/404.php';
+}
